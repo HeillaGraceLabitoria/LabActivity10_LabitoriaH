ae0cfa71c439d6dd20c9f5b20c13b1084b5c2cc5
MyUsername <hed-hlabitoria@smu.edu.ph>
2025-11-07 08:07:34 +0800

    fix

--- LabActivity10_LabitoriaH/Form1.vb
+++ LabActivity10_LabitoriaH/Form1.vb
@@ -4,37 +4,41 @@ Public Class Form1
     Dim filePath As String = "sample.txt"
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Try
-
-
-            Using writer As New StreamWriter(filePath, True) 'true append
-                ListBox1.Items.Add(NumericUpDown1.Text)
-
+            Using writer As New StreamWriter(filePath, True)
+                writer.WriteLine(NumericUpDown1.Value)
             End Using
-            MessageBox.Show("Data written successfully")
+            ListBox1.Items.Add(NumericUpDown1.Value)
+            MessageBox.Show("Data written successfully!")
         Catch ex As Exception
             MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
         End Try
     End Sub
-
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
-
-
-        Using reader As New StreamReader(filePath)
-            Dim content As String = reader.ReadToEnd
-            MessageBox.Show("File Content: " + content + ListBox1.Items.Add(NumericUpDown1.Text))
-
-        End Using
+        Try
+            If Not File.Exists(filePath) Then
+                File.Create(filePath).Close()
+            End If
+            Using reader As New StreamReader(filePath)
+                Dim content As String = reader.ReadToEnd
+                MessageBox.Show("File Content:" & vbCrLf & content)
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
-
     Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
-        ListBox1.Items.Clear()
-        Using reader As New StreamReader(filePath)
-            Dim line As String
-            line = reader.ReadLine()
-            While (line IsNot Nothing)
-                ListBox1.Items.Add(line)
-                line = reader.ReadLine()
-            End While
-        End Using
+        Try
+            If Not File.Exists(filePath) Then
+                File.Create(filePath).Close()
+            End If
+            ListBox1.Items.Clear()
+            Using reader As New StreamReader(filePath)
+                While Not reader.EndOfStream
+                    ListBox1.Items.Add(reader.ReadLine())
+                End While
+            End Using
+        Catch ex As Exception
+            MessageBox.Show(ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+        End Try
     End Sub
 End Class
